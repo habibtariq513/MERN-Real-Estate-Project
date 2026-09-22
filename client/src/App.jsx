a373bc2ccc59693161df55c29859f4cef6ad27cf
Habib Tariq <habibtariq513@example.com>
2026-09-22 20:12:15 +0500

    add Header component and integrate it into App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,9 +5,11 @@ import Home from './assets/pages/Home';
 import Profile from './assets/pages/Profile';
 import SignIn from './assets/pages/SignIn';
 import SignUp from './assets/pages/SignUp';
+import Header from './components/Header';
 
 export default function App() {
   return <BrowserRouter>
+    <Header/>
     <Routes>
       <Route path='/' element={<Home/>} />
       <Route path='/about' element={<About/>} />
