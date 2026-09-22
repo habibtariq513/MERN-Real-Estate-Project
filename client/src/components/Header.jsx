bb0776770ec2eeb919a65e6a4ee0510cb4a4d1af
Habib Tariq <habibtariq513@example.com>
2026-09-22 20:29:50 +0500

    refactor: update Header component structure and styling; adjust Tailwind CSS imports

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
 export default function Header() {
   return (
-    <div>
-      Header
-    </div>
-  )
+    <header>
+      <h1>
+        <span className="text-slate-500">Sahand</span>
+        <span className="text-slate-700">Estate</span>
+      </h1>
+    </header>
+  );
 }
