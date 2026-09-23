9be7fc830a6244bae7bf778ce9b5cc87a2b7832c
Habib Tariq <habibtariq513@example.com>
2026-09-23 16:56:21 +0500

    feat: add user routing and test controller for API

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from './routes/user.route.js';
+
+const app = express();
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -8,8 +11,9 @@ mongoose.connect(process.env.MONGO).then(() => {
 }).catch((err) => {
     console.log(err);
 });
-const app = express();
 
 app.listen(3000, () => {
     console.log("Server is running on Port 3000");
 });
+
+app.use('/api/user', userRouter);
