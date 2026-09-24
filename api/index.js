b105a3efac094943c329feb9025e169a955d834e
Habib Tariq <157604419+habibtariq513@users.noreply.github.com>
2026-09-24 16:13:11 +0000

    enhance error handling in signup controller and add centralized error middleware

--- api/index.js
+++ api/index.js
@@ -17,8 +17,20 @@ app.listen(3000, () => {
     console.log("Server is running on Port 3000");
 });
 
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+
 // It'll let the JSON data come to server
 app.use(express.json());
 
-app.use('/api/user', userRouter);
-app.use('/api/auth', authRouter);
+// middleware 
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
