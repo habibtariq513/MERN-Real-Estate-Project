7c4b5a373f2e2ba374edaebdbc902fa09c864526
Habib Tariq <157604419+habibtariq513@users.noreply.github.com>
2026-09-24 15:24:20 +0000

    feat: implement signup route and controller for user registration

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 const app = express();
 dotenv.config();
@@ -16,4 +17,8 @@ app.listen(3000, () => {
     console.log("Server is running on Port 3000");
 });
 
+// It'll let the JSON data come to server
+app.use(express.json());
+
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
